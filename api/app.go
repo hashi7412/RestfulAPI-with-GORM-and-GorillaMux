962ea2dd12b12787ad39694e5dbb9d5de86d75c0
hashi7412 <egghead.kenzo@gmail.com>
2023-07-27 14:35:33 -0500

    Update app

--- api/app.go
+++ api/app.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	handler "github.com/hashi7412/restfulapi-with-gorm-and-gorillamux/api/app"
 	"github.com/jinzhu/gorm"
 	"honnef.co/go/tools/config"
 )
@@ -40,4 +41,40 @@ func (a *App) setRouters() {
 
 func (a *App) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
 	a.Router.HandleFunc(path, f).Methods("PUT")
+}
+
+func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("DELETE")
+}
+
+func (a *App) GetAllEmployees(w http.ResponseWriter, r *http.Request) {
+	handler.GetAllEmployees(a.DB, w, r)
+}
+
+func (a *App) CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.CreateEmployee(a.DB, w, r)
+}
+
+func (a *App) GetEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.GetEmployee(a.DB, w, r)
+}
+
+func (a *App) UpdateEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.UpdateEmployee(a.DB, w, r)
+}
+
+func (a *App) DeleteEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.DeleteEmployee(a.DB, w, r)
+}
+
+func (a *App) DisableEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.DisableEmployee(a.DB, w, r)
+}
+
+func (a *App) EnableEmployee(w http.ResponseWriter, r *http.Request) {
+	handler.EnableEmployee(a.DB, w, r)
+}
+
+func (a *App) Run(host string) {
+	log.Fatal(http.ListenAndServe(host, a.Router))
 }
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	app "github.com/hashi7412/restfulapi-with-gorm-and-gorillamux/api"
+	config "github.com/hashi7412/restfulapi-with-gorm-and-gorillamux/api"
+)
+
 func main() {
 	config := config.GetConfig()
 
